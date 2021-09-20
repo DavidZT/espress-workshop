6ec63874b7ee0a2028d5cb61a76aa74e132bfb64
David Luna <dluna05@alumnos.uaq.mx>
2021-09-20 18:57:48 -0500

    Adding JWT

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const user =  express.Router();
+const jwt = require ('jsonwebtoken');
 const db = require('../config/database');
 
 user.post("/", async (req, res, next) => {
@@ -18,7 +19,27 @@ user.post("/", async (req, res, next) => {
     }
     return res.status(500).json({ code: 500, Message: "Campos Incompletos"});
 
-})
+});
+
+user.post("/login", async (req, res, next) => {
+    const { user_mail, user_password } = req.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
+    const rows = await db.query(query);
+
+    if(user_mail && user_password) {
+        if(rows.length == 1) {
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+            return res.status(200).json({code: 200, Message:"Aqui hira el token" });
+        }else{
+            return res.status(401).json({code: 401, Message:"Usuario y/o contraseña incorrecta" });
+        }
+    }
+    return res.status(500).json({code: 500, Message: "Campos Incompletos"});
+
+});
 
 user.get("/", async (req, res, next) => {
     const query = "SELECT * FROM user";
